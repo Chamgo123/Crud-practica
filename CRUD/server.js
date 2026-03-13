932bd40ab9394cacd083ceb1ab217b3fad322719
chamgo <chamgo123@outlook.com>
2026-03-12 22:24:34 -0600

    3.2

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const app = express();
 
 const distPath = path.join(__dirname, 'dist', 'CRUD', 'browser');
 
-console.log('Sirviendo archivos desde:', distPath);
-
 app.use(express.static(distPath));
 
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log('App ejecutándose en puerto: ' + port);
+    console.log(`Servidor listo en puerto ${port}`);
 });
