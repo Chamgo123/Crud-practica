e1f3434ae72b3567da137b0b04a274e48657c6e3
chamgo <chamgo123@outlook.com>
2026-03-12 21:57:47 -0600

    corregido

--- CRUD/server.js
+++ CRUD/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__directory + '/dist/CRUD'));
+app.use(express.static(path.join(__dirname, 'dist/CRUD')));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/CRUD/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/CRUD/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Aplicación corriendo en el puerto ${port}`);
+});
